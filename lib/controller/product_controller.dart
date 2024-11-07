dd0d45dcf6244875d9d1ee2758a0301ca55326fa
ahithmohan <ahitmohan123@gmail.com>
2024-11-07 16:01:42 +0530

    some issues from api data fetching

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -3,16 +3,23 @@ import 'dart:convert';
 import 'package:ecommerce/model/get_product_model.dart';
 import 'package:http/http.dart' as http;
 
+// class ProductController {
+//   Future<List<GetProductModel>> fetchData() async {
+//     final response =
+//         await http.get(Uri.parse("https://api.escuelajs.co/api/v1/products"));
+//
+//     if (response.statusCode == 200) {
+//       List<dynamic> data = json.decode(response.body);
+//       return data.map((json) => GetProductModel.fromJson(json)).toList();
+//     } else {
+//       throw Exception("Failed to load users");
+//     }
+//   }
+// }
 class ProductController {
-  Future<List<GetProductModel>> fetchData() async {
+  getData() async {
     final response =
         await http.get(Uri.parse("https://api.escuelajs.co/api/v1/products"));
-
-    if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body);
-      return data.map((json) => GetProductModel.fromJson(json)).toList();
-    } else {
-      throw Exception("Failed to load users");
-    }
+    print(response.statusCode);
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,10 @@
-import 'package:ecommerce/controller/product_controller.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-import '../model/get_product_model.dart';
+import 'package:dio/dio.dart';
+import 'package:ecommerce/model/get_product_model.dart';
+import 'package:ecommerce/model/product_response_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,14 +14,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final ProductController _productController = ProductController();
-  late Future<List<GetProductModel>> _productFuture;
+  ProductResponseModel productResponseModel = ProductResponseModel();
+  List<ProductResponseModel> products = [];
+  // getData() async {
+  //   final response = await Dio().get("https://fakestoreapi.com/products");
+  //   final responseBody = jsonDecode(response.data) as List;
+  //
+  //   final allProducts = responseBody['data']
+  //       .map((e) => ProductResponseModel.fromJson(e))
+  //       .toList();
+  //
+  //   print(response.data);
+  //   productResponseModel = ProductResponseModel.fromJson(response.data);
+  //   // for (var i = 0; i < response.data.length; i++) {
+  //   //   productResponseModel = ProductResponseModel.fromJson(response.data[i]);
+  //   //   print("${productResponseModel.id} ${productResponseModel.title}");
+  //   //   // productResponseModel.products.toList();
+  //   // }
+  //   // print("-------------Length-------------");
+  //   // print(products.length);
+  // }
 
   @override
   void initState() {
-    // TODO: implement initState
-    _productFuture = _productController.fetchData();
     super.initState();
+    // getData();
   }
 
   @override
@@ -65,79 +85,65 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
-              child: FutureBuilder(
-            future: _productFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text("Error: ${snapshot.error}"));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text("No products found"));
-              } else {
-                final products = snapshot.data!;
-                return GridView.builder(
-                  padding: const EdgeInsets.all(8.0),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 8.0,
-                    crossAxisSpacing: 8.0,
-                    childAspectRatio: 0.8,
+            child: GridView.builder(
+              padding: const EdgeInsets.all(8.0),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: products.length, // Number of items to display
+              itemBuilder: (context, index) {
+                return Card(
+                  color: const Color(0xffF8F8F8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  itemCount: products.length, // Number of items to display
-                  itemBuilder: (context, index) {
-                    return Card(
-                      color: const Color(0xffF8F8F8),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      elevation: 9,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                  elevation: 9,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              "assets/images/image 268.png",
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Leather Sofa",
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                        const SizedBox(height: 4),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Expanded(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Image.asset(
-                                  "assets/images/image 268.png",
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 8),
                             const Text(
-                              "Leather Sofa",
-                              style: TextStyle(fontWeight: FontWeight.w700),
+                              "1500/-",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w900),
                             ),
-                            const SizedBox(height: 4),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  "1500/-",
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w900),
-                                ),
-                                Image.asset(
-                                  "assets/images/Group 53.png",
-                                  height: 24,
-                                  width: 24,
-                                ),
-                              ],
+                            Image.asset(
+                              "assets/images/Group 53.png",
+                              height: 24,
+                              width: 24,
                             ),
                           ],
                         ),
-                      ),
-                    );
-                  },
+                      ],
+                    ),
+                  ),
                 );
-              }
-            },
-          )),
+              },
+            ),
+          ),
         ],
       ),
     );
